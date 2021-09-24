e5a71d6521300f2d35d18d715c201441657e96fa
Rahma0911 <88459971+Rahma0911@users.noreply.github.com>
2021-09-24 11:14:50 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-
+import picture from './picture.jpg';
 
 class App extends Component {
   state = {
     person: {fullName: 'Rahma Ismail',
-    bio: 'I’m a web developer. I build websites that delight and inform. I do it well.' , 
+    bio: 'I’m a web developer. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript. I build websites that delight and inform. I do it well. I’m curious, and I enjoy work that challenges me to learn something new and stretch in a different direction.' , 
+    imgSrc:<img src={picture} alt='profil' width='10%'></img>,
     profession: 'Web Developer'} ,
     isVisible: false
   }
@@ -18,6 +19,7 @@ class App extends Component {
         {
           this.state.isVisible ? ( <ul> <li>{this.state.person.fullName}</li>
           <li>{this.state.person.bio}</li>
+          <li>{this.state.person.imgSrc}</li>
           <li>{this.state.person.profession}</li>
           </ul> ) : 
           ( <h2> Click on the button to show profil </h2> )
